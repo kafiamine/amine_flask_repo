9f645acdca4602fbe06f31bf3f30bd215c160790
Vagrant Default User <vagrant@control>
2019-10-21 09:57:31 +0000

    corriger le fichier main pr etre standard avec pylint

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+"""ce fichier de test de la librairie flask"""
 from flask import Flask
 APP = Flask(__name__)
 @APP.route('/')
 def hello():
+    """fonction d'affichage basic"""
     return 'hello world'
 if __name__ == '__main__':
     APP.run(host="0.0.0.0")
